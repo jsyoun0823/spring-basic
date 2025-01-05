70cde6c679b689113c018d3fb42550dd94819bdd
jsyoun0823 <yjsun_@naver.com>
2025-01-05 14:37:19 +0900

    스프링 컨테이너, 스프링 빈 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //spring 컨테이너 (객체들을 관리) , 어노테이션 기반으로 AppConfig에 있는 정보를 가지고 관리하겠다.
+        // == Bean 어노테이션 붙은 애들을 스프링 컨테이너에 넣고 관리하겠다)
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
